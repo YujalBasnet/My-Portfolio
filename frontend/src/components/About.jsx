a0f2a588d81f0d446eda2a338e1ec417b9d24e1a
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-11 21:18:59 +0545

    Intial About_Section Added

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,29 @@
+function About(){
+
+return(
+
+<section id="about"
+className="py-20 px-20 bg-slate-900 text-white">
+
+
+<h2 className="text-4xl font-bold text-center">
+About Me
+</h2>
+
+
+<p className="mt-10 text-center text-gray-300">
+
+I am a Computer Science student interested in
+full stack development. I enjoy creating applications
+and solving real-world problems.
+
+</p>
+
+
+</section>
+
+)
+
+}
+
+export default About;
