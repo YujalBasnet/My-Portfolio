f02ab91d257b97a7bbf3a9952e5920f215b6ceda
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-13 21:27:22 +0545

    Intregating real time Email Notification

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,11 +1,13 @@
 const Contact = require("../models/Contact");
 
+const transporter = require("../config/mail");
+
 
 
 exports.createContact = (req,res)=>{
 
 
-const data = {
+const data={
 
 name:req.body.name,
 
@@ -22,45 +24,73 @@ Contact.create(data,(err,result)=>{
 
 if(err){
 
-return res.status(500).json({
-
-message:"Error saving message"
-
-});
-
-}
-
-
-
-res.json({
-
-message:"Message sent successfully"
-
-});
-
-
-});
-
-
-};
-
-
-
-
-exports.getContacts = (req,res)=>{
-
-
-Contact.getAll((err,result)=>{
-
-
-if(err){
-
 return res.status(500).json(err);
 
 }
 
 
-res.json(result);
+
+const mailOptions={
+
+
+from:process.env.EMAIL_USER,
+
+
+to:process.env.EMAIL_USER,
+
+
+subject:"New Portfolio Contact Message",
+
+
+text:
+
+`
+You received a new message.
+
+Name:
+${data.name}
+
+
+Email:
+${data.email}
+
+
+Message:
+${data.message}
+
+`
+
+};
+
+
+
+transporter.sendMail(mailOptions,(error,info)=>{
+
+
+if(error){
+
+console.log(error);
+
+}
+
+else{
+
+console.log("Email sent");
+
+}
+
+
+});
+
+
+
+
+res.json({
+
+message:"Message saved and email sent"
+
+});
+
 
 
 });
