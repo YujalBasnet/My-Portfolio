99bd7c17dfa90a8fff2a05b677cbd78066ea3ead
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-14 15:13:17 +0545

    Implementing Mobile Hamburger Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import {motion} from "framer-motion";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 
 function Navbar(){
 
+
+const [open,setOpen] = useState(false);
+
+
+const links=[
+
+"Home",
+"About",
+"Skills",
+"Projects",
+"Contact"
+
+];
+
+
+
 return(
+
 
 <motion.nav
 
@@ -12,23 +30,28 @@ initial={{y:-100}}
 animate={{y:0}}
 
 className="
-fixed top-0
+fixed
+top-0
+left-0
 w-full
 z-50
 bg-slate-950/80
 backdrop-blur-lg
-border-b
-border-slate-800
-px-8
-py-5
-flex
-justify-between
-items-center
 text-white
+px-6
+md:px-20
+py-5
 "
 
 
 >
+
+
+<div className="
+flex
+justify-between
+items-center
+">
 
 
 <h1 className="
@@ -43,24 +66,25 @@ Yujal.
 
 
 
+{/* Desktop Menu */}
+
 <div className="
-flex
+hidden
+md:flex
 gap-8
-text-gray-300
 ">
 
 
 {
-["Home","About","Skills","Projects","Contact"]
 
-.map((item)=>(
+links.map(link=>(
 
 
 <a
 
-key={item}
+key={link}
 
-href={`#${item.toLowerCase()}`}
+href={`#${link.toLowerCase()}`}
 
 className="
 hover:text-cyan-400
@@ -69,7 +93,7 @@ transition
 
 >
 
-{item}
+{link}
 
 </a>
 
@@ -79,8 +103,104 @@ transition
 }
 
 
+</div>
+
+
+
+
+
+{/* Mobile Button */}
+
+<button
+
+
+onClick={()=>setOpen(!open)}
+
+
+className="
+md:hidden
+text-3xl
+"
+
+
+>
+
+
+{open ? "✕" : "☰"}
+
+
+</button>
+
+
 
 </div>
+
+
+
+
+
+{/* Mobile Menu */}
+
+
+{
+
+open && (
+
+
+<div
+
+className="
+md:hidden
+mt-5
+flex
+flex-col
+gap-5
+bg-slate-900
+p-5
+rounded-lg
+"
+
+
+>
+
+
+{
+
+links.map(link=>(
+
+
+<a
+
+key={link}
+
+href={`#${link.toLowerCase()}`}
+
+onClick={()=>setOpen(false)}
+
+className="
+hover:text-cyan-400
+"
+
+>
+
+{link}
+
+</a>
+
+
+))
+
+
+}
+
+
+
+</div>
+
+
+)
+
+}
 
 
 
