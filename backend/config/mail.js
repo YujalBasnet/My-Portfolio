988eb01c5f49eace25cf31887e0dc1f14481de90
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-18 15:34:09 +0545

    debug email

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,19 +1,14 @@
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-
-    service:"gmail",
-
-    auth:{
-
-        user:process.env.EMAIL_USER,
-
-        pass:process.env.EMAIL_PASS
-
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
     }
-
 });
-
 
 module.exports = transporter;
