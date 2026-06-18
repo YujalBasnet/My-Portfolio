898913713d0c8e6d7754e1b96361875fe4732121
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-18 15:44:02 +0545

    debug email

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 console.log("mail.js loaded");
 
-const nodemailer = require("nodemailer");
+
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
