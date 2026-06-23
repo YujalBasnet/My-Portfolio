94f17b5437052bab26e4bc50536c08a9020d67f3
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-23 15:40:03 +0545

    feat(backend): Real time email sent bug fixed!!

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,25 +1,20 @@
 const nodemailer = require("nodemailer");
-const dns = require("dns");
-
-dns.setDefaultResultOrder("ipv4first");
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+
+    service: "gmail",
+
+    auth: {
+
+        user: process.env.EMAIL_USER,
+
+        pass: process.env.EMAIL_PASS
+
+    }
+
 });
 
-transporter.verify((error) => {
-  if (error) {
-    console.log("SMTP Verify Error:", error);
-  } else {
-    console.log("SMTP Server Ready");
-  }
-});
 
 module.exports = transporter;
