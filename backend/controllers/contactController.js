16813ad3c2870d45e7c206fbf60bff6fedd33d4e
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-23 15:41:11 +0545

    feat(Backend): Bug Fixed

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,50 +1,115 @@
-exports.createContact = async (req, res) => {
-  try {
-    const { name, email, message } = req.body;
+const transporter= require("../config/mail");
 
-    // Validate empty fields
-    if (!name?.trim() || !email?.trim() || !message?.trim()) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
-    }
 
-    // Validate email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+exports.createContact = async (req,res)=>{
 
-    if (!emailRegex.test(email.trim())) {
-      return res.status(400).json({
-        message: "Please enter a valid email address",
-      });
-    }
 
-    // Send email
-    const response = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "laxmikhulalbasnet@gmail.com",
-      subject: "New Portfolio Message",
-      text: `
-New message received
+try{
 
-Name: ${name}
-Email: ${email}
+
+const {name,email,message}=req.body;
+
+
+
+if(
+!name?.trim() ||
+!email?.trim() ||
+!message?.trim()
+){
+
+return res.status(400).json({
+
+message:"All fields are required"
+
+});
+
+}
+
+
+
+
+const emailRegex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+
+if(!emailRegex.test(email.trim())){
+
+
+return res.status(400).json({
+
+message:"Invalid email"
+
+});
+
+}
+
+
+
+
+await transporter.sendMail({
+
+
+from: process.env.EMAIL_USER,
+
+
+to: process.env.EMAIL_USER,
+
+
+subject:"New Portfolio Message",
+
+
+text:`
+
+New message from portfolio
+
+
+Name:
+${name}
+
+
+Email:
+${email}
+
 
 Message:
 ${message}
-      `,
-    });
 
-    console.log("Email sent:", response);
 
-    return res.status(200).json({
-      message: "Message sent successfully",
-    });
+`
 
-  } catch (error) {
-    console.error("Resend Error:", error);
 
-    return res.status(500).json({
-      message: "Failed to send message",
-    });
-  }
+});
+
+
+
+
+console.log("Email sent successfully");
+
+
+
+res.status(200).json({
+
+message:"Message sent successfully"
+
+});
+
+
+
+}
+
+catch(error){
+
+
+console.log("Email Error:",error);
+
+
+res.status(500).json({
+
+message:"Failed to send email"
+
+});
+
+
+}
+
+
 };
