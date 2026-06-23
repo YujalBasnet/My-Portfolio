822a662c41ba06693ea3fa814fb529d391a15975
Yujal <laxmikhulalbasnet@gmail.com>
2026-06-23 16:33:00 +0545

    Fixing bugs

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,19 +1,21 @@
 const nodemailer = require("nodemailer");
 
-
 const transporter = nodemailer.createTransport({
-
-    service: "gmail",
-
-    auth: {
-
-        user: process.env.EMAIL_USER,
-
-        pass: process.env.EMAIL_PASS
-
-    }
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Verify Error:", error);
+  } else {
+    console.log("SMTP Ready");
+  }
+});
 
 module.exports = transporter;
